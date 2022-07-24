5545b96eaa62d2b9a825c5c104ca6be286b783c3
Gaeta Pamela <odontologia.pamela@gmail.com>
2022-07-24 08:51:25 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,29 @@
+ // Crie um array que receba 5 itens e exiba no console.
+ // 0 1 2 3 4 
+ let amigos = ["Pamela", "Henry", "Paloma", "Beatriz", "Marcio"];
+ console.log(amigos);
+ console.log(amigos[3]);
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+amigos.unshift("Carol")
+console.log("Método para adicionar um nome ao inicio do array:", amigos);
+
+// Utilize um método para remover o último nome do array.
+amigos.pop();
+console.log ("Método para remover o último nome do array:",amigos);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+amigos.push("Raquel", "Maria");
+console.log("Adicionar dois nomes ao fim do array", amigos);
+
+
+// Utilize um método para remover o primeiro nome do array.
+amigos.shift();
+console.log("Método para remover o primeiro nome do array.:",amigos);
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+let numbers = [7,5,6,3,8,9,2,1,4]
+
+console.log();
